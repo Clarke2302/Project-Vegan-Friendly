1465c117a878f55fefdac8cf5e5253359f5226b6
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2024-02-29 22:18:12 -0300

    Criado classe fibo e adicionei arrays

---
+++ Fibo.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main (String[] args){
+
+        //criação da array
+        int [] numeros = new int [5];
+        String [] nomes = new String [5];
+
+
+    }
+}
